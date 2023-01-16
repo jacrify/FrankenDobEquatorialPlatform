b49a240265616997ac866945fe797c556642ac68
John Cranney <john.cranney@gmail.com>
2023-01-17 10:32:11 +1100

    Fixed comment

--- src/EquatorialPlatform.cpp
+++ src/EquatorialPlatform.cpp
@@ -1,12 +1,13 @@
 #include "DigitalCaliper.h"
+#include "EQWebServer.h"
 #include "Logging.h"
 #include "Network.h"
 #include "OTA.h"
-#include "EQWebServer.h"
 
-# Radius of circle from platform rotaion axis to caliper strip is 230.5mm
-# Therefore circumference of that circle is 1448.3mm
-# Therefore target speed along the strip is 1mm per minute (1.006!) : 1488.3 / 24/60
+// Radius of circle from platform rotaion axis to caliper strip is 230.5mm
+// Therefore circumference of that circle is 1448.3mm
+// Therefore target speed along the strip is 1mm per minute (1.006!) : 1488.3 /
+// 24/60
 
 void setup() {
   setupWifi();
@@ -18,6 +19,5 @@ void setup() {
   Serial.begin(115200);
   Serial.println("Booting");
 }
-
 
 void loop() { loopOTA(); }
