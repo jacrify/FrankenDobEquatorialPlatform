89fa3519e02db3363fe9d0a9a575f6f59737bb4c
John Cranney <john.cranney@gmail.com>
2023-01-16 16:59:51 +1100

    Calculated required speed

--- src/EquatorialPlatform.cpp
+++ src/EquatorialPlatform.cpp
@@ -4,6 +4,10 @@
 #include "OTA.h"
 #include "EQWebServer.h"
 
+# Radius of circle from platform rotaion axis to caliper strip is 230.5mm
+# Therefore circumference of that circle is 1448.3mm
+# Therefore target speed along the strip is 1mm per minute (1.006!) : 1488.3 / 24/60
+
 void setup() {
   setupWifi();
   setupOTA();
@@ -14,5 +18,6 @@ void setup() {
   Serial.begin(115200);
   Serial.println("Booting");
 }
+
 
 void loop() { loopOTA(); }
