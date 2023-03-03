800bfe78e993049130f653b0eaabef231e005f22
John Cranney <john.cranney@gmail.com>
2023-03-04 09:38:24 +1100

    Workng caliper

--- src/DigitalCaliper.cpp
+++ src/DigitalCaliper.cpp
@@ -10,6 +10,8 @@ long packet = 0;
 long microns = 0;
 std::string bitString = "";
 
+int sampleCount = 0;
+int windowSize=STARTING_WINDOW_SIZE;
 
 // convert a packet to signed microns
 long DigitalCaliper::getMicrons(long packet) {
@@ -23,9 +25,7 @@ long DigitalCaliper::getMicrons(long packet) {
   return data;
 }
 
-void IRAM_ATTR clockFall() {
-  DigitalCaliper::dpInstance->interruptHandler();
-}
+void IRAM_ATTR clockFall() { DigitalCaliper::dpInstance->interruptHandler(); }
 
 void DigitalCaliper::interruptHandler() {
   int data = digitalRead(DATAPIN);
@@ -46,9 +46,9 @@ void DigitalCaliper::interruptHandler() {
     cycleStart = micros();
     bitIndex = 0;
 
-    loga(&bitString[0]);
-    microns=getMicrons(packet);
-    loga("Packet Value: %d which is microns: %d", packet, microns);
+    // loga(&bitString[0]);
+    microns = getMicrons(packet);
+    // loga("Packet Value: %d which is microns: %d", packet, microns);
     packet = 0;
     bitString = "";
   }
@@ -58,7 +58,7 @@ void DigitalCaliper::interruptHandler() {
 DigitalCaliper::DigitalCaliper() { dpInstance = this; }
 
 void DigitalCaliper::setUp() {
-  
+
   pinMode(DATAPIN, INPUT);
   pinMode(CLOCKPIN, INPUT);
 
@@ -77,10 +77,11 @@ void DigitalCaliper::reset() {
   dacWrite(DACPIN, 0);
   delay(ONOFFDELAY);
   dacWrite(DACPIN, DACLEVEL);
-  // clear();
+  clear();
   // vTaskResume(TaskHandle);
 }
 void DigitalCaliper::setWindowSize(int size) {
+  windowSize=size;
   positions.resize(size);
   times.resize(size);
   velocities.resize(size);
@@ -92,6 +93,7 @@ void DigitalCaliper::clear() {
   times.clear();
   velocities.clear();
   errorCount = 0;
+  sampleCount = 0;
 }
 
 void DigitalCaliper::sleepBetweenSamples() { delay(SAMPLEDELAY); }
@@ -111,26 +113,48 @@ void DigitalCaliper::takeSample() {
     errorCount++;
     return;
   }
-
-  times.push(now);
-  positions.push(microns);
-
-  if (times.size() > 0) {
-    unsigned long deltaTime = now - times.back(); // oldest time
-    long deltaPos = microns - positions.back();
-
-    if (abs(deltaPos) < SENSIBLE_MOVEMENT) {
-      log("Dropping sample as out of sensible movement range, actual position "
-          "reported is %d",
-          microns);
+  sampleCount++;
+  if (sampleCount == 1) {
+    // can't check for jumps on first sample, just need to grab it and hope it's
+    // not noisy
+    times.push(now);
+    positions.push(microns);
+    log("Building initial time samples , position %d", microns);
+  } else {
+    long deltaToLast = positions.front() - microns;
+    if (abs(deltaToLast) > SENSIBLE_SHIFT) {
+      log("Dropping sample as shift is too far "
+          "position "
+          "reported is %d, which makes change in pos %d  ",
+          microns, deltaToLast);
       errorCount++;
       return;
     }
 
+    unsigned long deltaTime = now - times.back(); // oldest time
+    long deltaPos = microns - positions.back();
+
     float velocity =
-        (float)deltaPos / (float)deltaTime * 100.0; // microns per second ??
+        (float)deltaPos / (float)deltaTime *1000; // mm per minute ??
     log("Delta time: %d \t delta pos %d \t current pos %d \t Velocity: %f",
         deltaTime, deltaPos, microns, velocity);
-    velocities.push(velocity);
+
+    // if (abs(velocity) > SENSIBLE_SPEED) {
+    //   log("Dropping sample as speed is too fast, actual "
+    //       "position "
+    //       "reported is %d, which makes change in pos %d and velocity ",
+    //       microns, deltaPos, velocity);
+    //   errorCount++;
+    //   return;
+    // }
+    times.push(now);
+    positions.push(microns);
+    // only store velocities when sample rate high enough
+    if (sampleCount > MIN_SAMPLES_FOR_VELOCITY) {
+
+      velocities.push(velocity);
+    } else {
+      log("Skipping velocity due to low sample count %d",sampleCount);
+    }
   }
 }
--- src/EquatorialPlatform.cpp
+++ src/EquatorialPlatform.cpp
@@ -14,6 +14,8 @@ DigitalCaliper caliper;
 // Therefore circumference of that circle is 1448.3mm
 // Therefore target speed along the strip is 1mm per minute (1.006!) : 1488.3 /
 // 24/60
+// In micros per second this is:
+// 1488.3/24/60/60*100 = 1.72256944
 
 // void sampleLoop(void *parameter) {
 //   // DigitalCaliper *caliper = (DigitalCaliper *)parameter;
