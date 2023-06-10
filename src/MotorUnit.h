391681895487ee8c3cbe779bddf193339f83bc0d
John Cranney <john.cranney@gmail.com>
2023-06-10 10:15:16 +1000

    Broken comiple

--- src/MotorUnit.h
+++ src/MotorUnit.h
@@ -1,7 +1,22 @@
 #ifndef MOTORUNIT_H
 #define MOTORUNIT_H
 
-void setupMotor();
-void onLoop();
+class MotorUnit {
+public:
+  void setupMotor();
+  void onLoop();
 
+  double getGreatCircleRadius();
+  void setGreatCircleRadius(double radius);
+
+  int getCalibrationSpeed();
+  void setCalibrationSpeed(int speed);
+
+  int getRunBackSpeed();
+  void setrunbackSpeed(int speed);
+
+  int getLimitSwitchToEndDistance();
+
+  void moveTo(int location);
+};
 #endif
