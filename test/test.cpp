c25816ff193029ca26207038e553f547e76be050
John Cranney <john.cranney@gmail.com>
2023-06-15 08:07:46 +1000

    Added move controls, test case poc

--- test/test.cpp
+++ test/test.cpp
@@ -1,2 +1,26 @@
-#include <iostream>
-using namespace std;
+#include "MotorUnit.h"
+#include <unity.h> // Include the Unity test framework.
+
+
+
+void test_motor_unit_speed(void) {
+  
+  TEST_ASSERT_EQUAL_FLOAT(2,
+                          3); // Replace 'expected_value' with
+                                               // the value you're expecting.
+}
+
+void setup() {
+  UNITY_BEGIN(); // IMPORTANT LINE!
+  RUN_TEST(test_motor_unit_speed);
+  UNITY_END(); // IMPORTANT LINE!
+}
+
+void loop() {
+  // Do nothing here.
+}
+
+int main() {
+  setup();
+return 0;
+}
