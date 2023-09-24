5ea103ff02c7c6c6038c26b30efec1bae532831f
John Cranney <john.cranney@gmail.com>
2023-09-25 08:21:58 +1000

    Removed web serial

--- lib/Logging/src/Logging.cpp
+++ lib/Logging/src/Logging.cpp
@@ -8,7 +8,7 @@
 #include <Arduino.h>
 #endif
 bool webSerialReady;
-void setWebSerialReady() { webSerialReady = true; }
+
 void log(const char *fmt, ...) {
   const int BUFFER_SIZE = 1024;
   char buffer[BUFFER_SIZE];
@@ -23,8 +23,6 @@ void log(const char *fmt, ...) {
 #ifdef ARDUINO
   // If we're on an Arduino (or compatible) platform
   Serial.println(buffer);
-  if (webSerialReady)
-    WebSerial.println(buffer);
 #else
   // For native environment
   std::cout << buffer << std::endl; // Print to console
